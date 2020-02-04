59d00c5e8f167c58980994dad78ee7f64694a910
thetntm <michael.becker.123@gmail.com>
2020-02-03 22:43:57 -0800

    initial commit

---
+++ mainJS.js
@@ -0,0 +1,32 @@
+//Global Variables
+/* 
+Put all variables that will be usedthroughout the js file Here.
+If Variables are used only in one function, declare them within that function's scope.
+*/
+
+//DOM Elements and Jquery Wrappers
+/*
+If a DOM Element or Jquery Wrapper is important to the project, declare it as a variable here.
+*/
+
+//Useful Functions
+/*
+This is where we will define functions that we may be calling often,
+or that would be useful to have defined as functions instead of standard code.
+*/
+
+//Event Functions
+/*
+This is where we will define functions that are called by event handlers,
+Such as click methods for buttons
+*/
+
+//Event Assignment
+/*
+This is where we will assign the events of various elements to their functions.
+*/
+
+//Code to run on Page load
+/*
+This is where we will put any code that needs to be run after the page has loaded.
+*/
